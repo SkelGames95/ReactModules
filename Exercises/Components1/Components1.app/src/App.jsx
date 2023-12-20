1d071370b718949b61258e710856954dfa847954
Marco.Polenta <polentamarco.dev@gmail.com>
2023-12-20 21:20:06 +0100

    Components1 complete

---
+++ Exercises/Components1/Components1.app/src/App.jsx
@@ -0,0 +1,9 @@
+import { Hello } from "./Hello";
+
+export const App = () => {
+    return (
+        <div>
+            <Hello />
+        </div>
+    )
+}
---
+++ Exercises/Components1/Components1.app/src/Hello.jsx
@@ -0,0 +1,1 @@
+export const Hello = () => <h2>Hello World!</h2>;
