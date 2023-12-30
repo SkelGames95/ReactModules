15a677e6cb43faeda5c44952cbf2843aba4edc22
Marco.Polenta <polentamarco.dev@gmail.com>
2023-12-30 17:41:50 +0100

    Conditional Rendering lesson done

--- Lessons/my-app/src/Welcome.jsx
+++ Lessons/my-app/src/Welcome.jsx
@@ -1,3 +1,5 @@
+import { HelloWorld } from "./HelloWorld"
+
 // FIRST LESSONS ON JSX AND PROPS
 
 // accessing object property directly on result
@@ -34,17 +36,30 @@
 
 // let's see how the conditional rendering works:
 
-export const Welcome = ({name, age}) => {
-    return (<div>
-        <p>Welcome, <strong>{name}</strong>!</p>
-        {age > 18 && <p>Your age is: {age}.</p>}
-        {age < 18 && <p>You are too young to go further!</p> }
-    </div>    
-)}
+// export const Welcome = ({name, age}) => {
+//     return (<div>
+//         <p>Welcome, <strong>{name}</strong>!</p>
+//         {age && <p>Your age is decleared.</p>}
+//         {!!age && <p>Your age isn't decleared.</p>}
+//         {age > 18 && <p>Your age is: {age}.</p>}
+//         {age < 18 && <p>You are too young to go further!</p> }
+//     </div>    
+// )}
 
-Welcome.defaultProps = {name: "guest", /*age: "undefined"*/};
-
+// Welcome.defaultProps = {name: "guest", /*age: "undefined"*/};
 
 /* embedding a portion of a component expression with {} means to be able to use it for conditional rendering depending on the condition assigned 
 in this case, if age is true = render the paragraph "your age is" */
 /* if a default prop being truthy is set, that value will be rendered substituting the lack of data */
+/* made some examples to show how to use Conditional Rendering */
+
+
+// Conditional Rendering with Ternary Operator "?" in "Welcome"
+
+export const Welcome = ({name, age}) => {
+    return (<div>
+        {name ? <p>Welcome, <strong>{name}</strong>!</p> : <HelloWorld />}
+        {age > 18 && <p>Your age is: {age}.</p>}
+        {age < 18 && <p>You are too young to go further!</p> }
+    </div>    
+)}
