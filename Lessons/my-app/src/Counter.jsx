612c80940f6d2d8f2c463745d4da1c38c07e9efa
Marco.Polenta <polentamarco.dev@gmail.com>
2024-01-06 02:55:39 +0100

    useEffect tried in Counter,jsx

--- Lessons/my-app/src/Counter.jsx
+++ Lessons/my-app/src/Counter.jsx
@@ -1,6 +1,6 @@
 // Going to create a component with counter applying hooks for the firts time, specifically using the useState hook
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 // export const Counter = () => {
 //     const [ counter, counterUpdate ] = useState(10);        // using the destructuring to assign variables to useState values
@@ -36,9 +36,48 @@ import { useState } from "react"
 // fact: to put a placeholder in this code we can conventionally write it like _THIS_
 
 
-export const Counter = ({initialValue = 0}) => {                                        // passing initialValue with default value = 0 as a prop 
-    const [ counter, counterUpdate ] = useState(initialValue);                          // passing the initialValue as basic counter value
+// export const Counter = ({initialValue = 0}) => {                                        // passing initialValue with default value = 0 as a prop 
+//     const [ counter, counterUpdate ] = useState(initialValue);                          // passing the initialValue as basic counter value
 
+//     function handleCounterIncrement () {
+//         counterUpdate((c) => c + 1);
+//         counterUpdate((c) => c + 1);
+//         counterUpdate((c) => c + 1);
+//     }
+
+//     function handleCounterDecrement () {
+//         counterUpdate(counter - 1)
+//     }
+
+//     function handleResetCounter () {
+//         counterUpdate(initialValue)                                                     // passing initialValue as the reset value
+//     }
+
+//     return (
+//         <div>
+//             <p>I have counted to {counter}</p>
+//             <button onClick={handleCounterIncrement}>Increment</button>
+//             <br/>
+//             <br/>
+//             <button onClick={handleCounterDecrement}>Decrement</button>
+//             <br/>
+//             <br/>
+//             <button onClick={handleResetCounter}>Reset</button>
+//         </div>
+//     )
+// }
+
+
+
+// Adding useEffect hook to the counter:
+
+export const Counter = ({initialValue = 0}) => {
+    const [ counter, counterUpdate ] = useState(initialValue);
+
+    // useEffect(() => {
+    //     console.log(`The value of the counter is ${counter}`);                          // passing the effect (function) as the first parameter and the value to control as the second one
+    // }, [counter, initialValue])                                                         // so every time the counter changes the app execute the function to print on console the message
+                                                                                        // I can also add another value to the dependency array to trigger the effect whenever one of them changes
     function handleCounterIncrement () {
         counterUpdate((c) => c + 1);
         counterUpdate((c) => c + 1);
@@ -50,7 +89,7 @@ export const Counter = ({initialValue = 0}) => {                                
     }
 
     function handleResetCounter () {
-        counterUpdate(initialValue)                                                     // passing initialValue as the reset value
+        counterUpdate(initialValue)
     }
 
     return (
