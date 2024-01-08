785ca079b54baaf01505b7f2e1046fdbb9308f13
Marco.Polenta <polentamarco.dev@gmail.com>
2024-01-08 12:01:14 +0100

    Handling Events 2 done

--- Exercises/ReactApp/src/MouseClicker.jsx
+++ Exercises/ReactApp/src/MouseClicker.jsx
@@ -4,9 +4,19 @@ export const MouseClicker = () => {
         console.log(event.target.name)
     }
 
+    const handleImgOnClick = (event) => {
+        console.log(event.target.src)
+    }
+
     return (
         <div>
-            <button name="one" onClick={handleButtonOnClick}>Click me!</button>
+            <button name="one" onClick={handleButtonOnClick}>
+                <img src="https://cdn.icon-icons.com/icons2/562/PNG/512/on-off-power-button_icon-icons.com_53938.png" width={25} height={25} onClick={handleImgOnClick}/>
+                Click me!
+            </button>
         </div>
     )
 }
+
+// The name of the button would appear if I would use the same event handler for both the elements because of bubbling
+// or using event.currentTarget.name as the parameter for the button event handler. In this way it will not appear by clicking the image.
