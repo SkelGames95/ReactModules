3a4a2c251c6810210df6ce09f1be222028e9fb71
Marco.Polenta <polentamarco.dev@gmail.com>
2024-01-17 03:22:24 +0100

    mod to uncontrolled forms

--- Lessons2/ReactApp3/src/UncontrolledForm.jsx
+++ Lessons2/ReactApp3/src/UncontrolledForm.jsx
@@ -20,7 +20,7 @@ export const UncontrolledForm = () => {
         <form onSubmit={handleFormSubmit}>
             <label>Username:<input name="username" placeholder="Username"/></label><br/><br/>
             <label>Password:<input name="password" type="password" placeholder="Password"/><br/></label>
-            <label for="password">*1 to 8 alphanumeric characters and symbols with at least one for each of them</label><br/><br/>
+            <label>*1 to 8 alphanumeric characters and symbols with at least one for each of them</label><br/><br/>
             <label><input name="session" type="checkbox"/>Remember me</label><br/><br/>
             <button name='login' type="submit">Login</button><hr/>
             <button name='reset' type="reset">Reset</button>
